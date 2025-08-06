6a80a9ca012344b07cd1805bb6f1e848957e899f
wpsadi <wpsadi@outlook.com>
2025-08-07 02:17:16 +0530

    save

--- src/app/(pages)/configure/complete-configuration/page.tsx
+++ src/app/(pages)/configure/complete-configuration/page.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import type React from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-function Page({ className, ...props }: React.ComponentProps<"div">) {
+function Page() {
 	return (
-		<div className={cn("flex flex-col gap-6", className)} {...props}>
+		<div className={cn("flex flex-col gap-6")}>
 			<div>
 				<div className="flex flex-col gap-6">
 					<div className="flex flex-col items-center gap-2">
